a7582ec194f1c98e97542dabb7aa713514c18d2d
Murat <murat.cevik@tugay.com.tr>
2025-02-27 22:51:48 +0300

    Left Menu Added

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,29 @@
 import { Routes } from '@angular/router';
+import { LayoutComponent } from './layout/layout.component';
+import { inject } from '@angular/core';
+import { AuthService } from './services/auth.service';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+
+
+  {path:"login",loadComponent:()=>import('./login/login.component').then(m=>m.LoginComponent)},
+
+
+  {
+    path: '',
+    component: LayoutComponent, canActivate: [ () =>inject(AuthService).isAuthenticated() ],
+    children: [
+      { path: '', redirectTo: 'home', pathMatch: 'full' }, // Boş path olduğunda home'a yönlendir
+      { path: 'home', loadComponent: ()=>   import('./layout/home/home.component').then(m=>m.HomeComponent) },
+      { path: 'about', loadComponent: ()=>   import('./layout/about/about.component').then(m=>m.AboutComponent) },
+    ]
+
+  }
+
+
+
+
+
+
+];
---
+++ frontend/src/app/services/auth.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+
+  constructor(  private router:Router) {  }
+
+isAuthenticated(){
+
+  const token:string | null = localStorage.getItem('token');
+
+  if (!token){
+  this.router.navigate(['/login']);
+    return false;
+   }
+   else { return true;  }
+
+}
+
+
+
+
+
+}
